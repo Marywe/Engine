8e3c8a70a76609d086590ed476eea1110612ee6b
María <kirilopez99@gmail.com>
2022-01-18 12:20:11 +0100

    POCo a poco

---
+++ engine/projects/Demo/source/main.cpp
@@ -0,0 +1,24 @@
+
+// This is free code released into the public domain.
+// Drafted by Ángel in January 2019.
+// angel.rodriguez@esne.edu
+
+// SDL no soluciona del todo bien el problema de llamar a main() desde WinMain().
+// Esta es una alternativa que funciona y no da problemas.
+
+#if defined(_WIN32) || defined(_WIN64)
+
+#include <windows.h>
+#include <iostream>
+//#include "Window.hpp"
+using namespace std;
+extern int main(int, char**);
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
+{
+    //return main(__argc, __argv);
+    
+    return 0;
+}
+
+#endif
